f7f56d0ab261fa81312bc25e9f6ffb600863ef19
Kamal Kohli <kamal89kohli@gmail.com>
2024-07-14 21:14:00 +0000

    initialize the game board

--- run.py
+++ run.py
@@ -4,3 +4,13 @@ from random import randint
 from os import system, name
 
 colorama.init()
+
+# Initialize the game boards
+def init_boards(size):
+    return [['.' for _ in range(size)] for _ in range(size)]
+
+
+
+
+if __name__ == "__main__":
+    
