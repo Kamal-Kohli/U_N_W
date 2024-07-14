86882d1770be3f49bf37f35ce7925ee6d747ec0c
Kamal Kohli <kamal89kohli@gmail.com>
2024-07-14 21:08:16 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+import colorama
+from random import randint
+from os import system, name
+
+colorama.init()
